32d1f8c7cb60d0d2965a682932d1dc17ad5b0fa5
Timothy Reynolds <timgreynolds@icloud.com>
2024-02-22 18:58:45 -0500

    Changes to be committed: 	modified:   UnifiedLogging/UnifiedLogger.cs 	modified:   UnifiedLogging/UnifiedLoggerProvider.cs 	modified:   UnifiedLogging/UnifiedLogging.csproj 	modified:   UnifiedLogging/UnifiedLoggingExtensions.cs
    	deleted:    UnifiedLogging/UnifiedLoggingProvider.cs

--- UnifiedLogging/UnifiedLoggingExtensions.cs
+++ UnifiedLogging/UnifiedLoggingExtensions.cs
@@ -8,7 +8,7 @@ namespace com.mahonkin.tim.logging.UnifiedLogging.Extensions;
 
 public static class UnifiedLoggingExtensions
 {
-    private static ILoggingBuilder AddUnifiedLogger(this ILoggingBuilder builder)
+    public static ILoggingBuilder AddUnifiedLogger(this ILoggingBuilder builder)
     {
         builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<ILoggerProvider, UnifiedLoggerProvider>());
         LoggerProviderOptions.RegisterProviderOptions<UnifiedLoggerOptions, UnifiedLoggerProvider>(builder.Services);
@@ -23,18 +23,4 @@ public static class UnifiedLoggingExtensions
 
         return builder;
     }
-
-    // public static OSLogLevel MapOSLogLevel(this LogLevel logLevel)
-    // {
-    //     switch (logLevel)
-    //     {
-    //         case LogLevel.Critical: return OSLogLevel.Fault;
-    //         case LogLevel.Error: return OSLogLevel.Error;
-    //         case LogLevel.Warning: return OSLogLevel.Error;
-    //         case LogLevel.Information: return OSLogLevel.Info;
-    //         case LogLevel.Debug: return OSLogLevel.Debug;
-    //         case LogLevel.Trace: return OSLogLevel.Default;
-    //         default: return OSLogLevel.Default;
-    //     }
-    // }
 }
